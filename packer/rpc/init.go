3db8b480c122c3c2d513f7b7deda590e950f0636
Adrien Delorme <adrien.delorme@icloud.com>
2018-10-24 11:42:35 +0200

    comment duplicate gob registration

--- packer/rpc/init.go
+++ packer/rpc/init.go
@@ -3,7 +3,7 @@ package rpc
 import "encoding/gob"
 
 func init() {
-	gob.Register(new(map[string]interface{}))
+	// gob.Register(new(*map[string]interface{})) TODO(azr): may be just remove this line
 	gob.Register(new(map[string]string))
 	gob.Register(make([]interface{}, 0))
 	gob.Register(new(BasicError))
