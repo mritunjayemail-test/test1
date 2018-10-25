076dc7732bcb93b10f55dac8d6f8b087bd785a7d
Adrien Delorme <adrien.delorme@icloud.com>
2018-10-25 18:12:24 +0200

    add Debug/Force/OnError fields to config.Artifact
    * so they can be set per build artifact also

--- packer/config/root.go
+++ packer/config/root.go
@@ -14,6 +14,7 @@ type Root struct {
 }
 
 // Merge toMerge into root
+// slices will be appended together.
 func (root *Root) Merge(toMerge *Root) {
 	err := mergo.Merge(root, toMerge, mergo.WithAppendSlice)
 	if err != nil {
@@ -21,8 +22,19 @@ func (root *Root) Merge(toMerge *Root) {
 	}
 }
 
-// Artifact represents a basic packer artifact
+// Artifact represents the configuration for
+// a basic packer artifact run step.
+// It should be viewed as a config.Artifact
+// and not as a resulting artifact.
+//
+// All of it - except the Operation field -
+// can be set in config file.
+//
+// Pointer mark fields as not required
 type Artifact struct {
+	Debug        *bool         `hcl:"debug"`
+	Force        *bool         `hcl:"force"`
+	OnError      *string       `hcl:"on_error"`
 	Type         string        `hcl:"type,label"`
 	Name         string        `hcl:"name,label"`
 	Source       *string       `hcl:"source,attr"`
@@ -40,5 +52,5 @@ func (a *Artifact) FullName() string {
 // Provisioner represents a basic packer provisioner
 type Provisioner struct {
 	Type   string   `hcl:"type,label"`
-	Remain hcl.Body `hcl:",remain"` // remainin body will be used by implementers
+	Remain hcl.Body `hcl:",remain"` // remaining body will be used by implementers
 }
