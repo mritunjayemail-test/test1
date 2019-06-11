7f0e7fea186983f6803a701d88624b31e9a1aeb3
Adrien Delorme <adrien.delorme@icloud.com>
2019-06-11 16:05:27 +0200

    TimeoutProvisioner: give 1 minute to a timed out provisioner to return.

--- packer/provisioner_timeout.go
+++ packer/provisioner_timeout.go
@@ -14,30 +14,32 @@ type TimeoutProvisioner struct {
 }
 
 func (p *TimeoutProvisioner) Provision(ctx context.Context, ui Ui, comm Communicator) error {
-	ctx, cancel := context.WithTimeout(ctx, p.Timeout)
-	defer cancel()
-
-	// Use a select to determine if we get cancelled during the wait
-	ui.Say(fmt.Sprintf("Setting a %s timeout for the next provisioner...", p.Timeout))
-
-	errC := make(chan interface{})
-
+	errC := make(chan error)
 	go func() {
-		select {
-		case <-errC:
-			// all good
-		case <-ctx.Done():
-			switch ctx.Err() {
-			case context.DeadlineExceeded:
-				ui.Error("Cancelling provisioner after a timeout...")
-			default:
-				// the context also gets cancelled when the provisioner is
-				// successful
-			}
-		}
+		// init the timeout from the same goroutine
+		ctx, cancel := context.WithTimeout(ctx, p.Timeout)
+		defer cancel()
+
+		ui.Say(fmt.Sprintf("Setting a %s timeout for the next provisioner...", p.Timeout))
+		errC <- p.Provisioner.Provision(ctx, ui, comm)
 	}()
 
-	err := p.Provisioner.Provision(ctx, ui, comm)
-	close(errC)
-	return err
+	select {
+	case err := <-errC:
+		// all good
+		return err
+	case <-ctx.Done():
+		if ctx.Err() == context.DeadlineExceeded { 
+			ui.Error("Cancelling provisioner after a timeout...")
+		}
+		select {
+		case err := <-errC:
+			// provisioner probably returned after a cleanup
+			return err
+		//  give the provisioner 1 minute to cleanup/return
+		case <-time.After(1 * time.Minute):
+			ui.Error("Provisioner did not return/cleanup quick enough and is probably locked, aborting.")
+			return ctx.Err()
+		}
+	}
 }
